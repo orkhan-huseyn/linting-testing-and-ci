3342eda3091906a93376c7cb3eb1dd78d95b107f
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-06-04 21:08:58 +0100

    map done

---
+++ map.js
@@ -0,0 +1,8 @@
+const map = (arr, func) => {
+    let secondaryArr = [];
+    for (let elem of arr) {
+        let result = func(elem);
+        secondaryArr.push(result);
+    } 
+    return secondaryArr;
+}
