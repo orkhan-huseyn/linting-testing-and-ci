767358e4607c18cdcdfaadb32f38970d20af8ca6
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-06-04 20:54:24 +0100

    reverse done

---
+++ reverse.js
@@ -0,0 +1,11 @@
+const reverse = arr => {
+    if (arr === undefined){
+        return [];
+    }
+
+    let reversedArr = [];
+    for (let i = arr.length - 1; i >= 0; i--){
+        reversedArr.push(arr[i]);
+    }
+    return reversedArr;
+} 
