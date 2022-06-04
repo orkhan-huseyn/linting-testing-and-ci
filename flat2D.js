1f017afe428aaf90c57ec377101ab71fbd6c2b22
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-06-04 20:45:21 +0100

    flat2D done

---
+++ flat2D.js
@@ -0,0 +1,16 @@
+const flat2D = arr => {
+    if ((arr === undefined) || (arr.length === 0)) {
+        return [];
+    }
+
+    let secondaryArr = [];
+    for (let innerArr of arr) {
+        for (let elem of innerArr){
+            secondaryArr.push(elem);
+        }
+    }
+
+    return secondaryArr;
+}
+
+// rekursiv funksiya ile alinmadi :(
