7926b81ff9c7bb943f22dc4f537c0a475719d084
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-06-05 10:24:04 +0100

    every done

---
+++ every.js
@@ -0,0 +1,11 @@
+const every = (arr, func) => {
+    let istrue = true;
+
+    for (let elem of arr) {
+        let result = func(elem);
+        if (!result) {
+            istrue = false;
+        }
+    }
+    return istrue;
+}
