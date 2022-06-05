ea9dd11e4847f336c9545ba5f42fdc348e5546c8
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-06-05 11:46:41 +0100

    ceasarCipher done

---
+++ ceasarCipher.js
@@ -0,0 +1,7 @@
+const ceasarCipher = (str, num) => {
+    let encodedStr = '';
+    for (let i = 0; i < str.length; i++){
+        encodedStr += String.fromCharCode(str.charCodeAt(i) + num);
+    }
+    return encodedStr;
+}
