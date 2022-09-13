8d26c513f982a579a3323c001c9f8726a32ea46f
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2022-09-13 17:50:55 +0400

    fix this one

--- src/sum.js
+++ src/sum.js
@@ -1,7 +1,7 @@
 module.exports.sum = (list) => {
     if (list === undefined) return 0;
 
-    sumOfNums = 0;
+    let sumOfNums = 0;
     for (let listItem of list) {
         sumOfNums += listItem;
     }
